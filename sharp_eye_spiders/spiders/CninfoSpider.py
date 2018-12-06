6a2de0f6f9e72139b3cea3220b10e989cb63133e
Wu Tong <gutongsedeyouyu@163.com>
2018-12-06 13:56:33 +0800

    announcement

--- sharp_eye_spiders/spiders/CninfoSpider.py
+++ sharp_eye_spiders/spiders/CninfoSpider.py
@@ -19,9 +19,9 @@ class CninfoSpider(scrapy.Spider):
         all_announcements = json_loads(response.body.decode('utf-8'))['classifiedAnnouncements']
         for announcements in all_announcements:
             for announcement in announcements:
-                if AnnouncementFile.exists(self.db, announcement['adjunctUrl']):
+                file_url = 'http://www.cninfo.com.cn/{0}'.format(announcement['adjunctUrl'])
+                if AnnouncementFile.exists(self.db, file_url):
                     return
                 yield AnnouncementItem(security_code=announcement['secCode'], company_name=announcement['secName'],
                                        title=announcement['announcementTitle'], announcement_time=announcement['announcementTime'],
-                                       file_urls=['http://www.cninfo.com.cn/{0}'.format(announcement['adjunctUrl'])],
-                                       referer='')
+                                       file_urls=[file_url], referer='')
